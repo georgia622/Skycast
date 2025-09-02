80729d20fb23c997e8e1e9d1eedd34c9381c9373
georgia622 <s1295492@students.ncl-coll.ac.uk>
2025-09-02 17:20:08 +0100

    Variables added to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
 const apiKey = 'd1f3216ca51fd37fe4c4b5d2b22adbbc';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=wylam';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric';
 
 
-async function checkWeather(){
-    
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    let data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+ "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity +"%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
+
+checkWeather(city);
