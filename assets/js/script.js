a4b42df8c540693eab08835fc3729c4120f85d0b
georgia622 <s1295492@students.ncl-coll.ac.uk>
2025-09-02 16:52:21 +0100

    Weather api url added to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
 const apiKey = 'd1f3216ca51fd37fe4c4b5d2b22adbbc';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=wylam';
 
+
+async function checkWeather(){
+    
+}
