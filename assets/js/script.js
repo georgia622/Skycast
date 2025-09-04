1722556e7f363bf3ca36299f29a60a15a19244ff
georgia622 <s1295492@students.ncl-coll.ac.uk>
2025-09-04 16:21:24 +0100

    cityName variable created

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
-
+const cityName = document.querySelector(".city");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -13,7 +13,7 @@ async function checkWeather(city) {
     } else {
 
         let data = await response.json();
-        document.querySelector(".city").innerHTML = data.name;
+        cityName.innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
